19f9b61aea8be588bff77232f905ef5d6b8049de
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2024-03-23 10:52:00 +0530

    vercel analytics

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,12 +5,14 @@ import '../styles/LoginPage.css'
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { config } from "dotenv";
+import { Analytics } from "@vercel/analytics/react"
 // import { defineConfig } from "vite";
 function LoginPage(){
     return (
         <div className="mainSignInPage">
             <LeftDiv />
             <LoginForm />
+            <Analytics />
         </div>
     )
 }
