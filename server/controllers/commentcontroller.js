9597ec91a8c3ea4e2e7b0cd08a10eabb9cb99755
Mehmood-Deshmukh <deshmukhmehmood1@gmail.com>
2024-05-19 01:58:26 +0530

    Comments routes done!

---
+++ server/controllers/commentcontroller.js
@@ -0,0 +1,87 @@
+const Comment = require("../models/commentModel");
+
+async function getComments(req, res) {
+  try {
+    const postId = req.query.postId;
+    const comments = await Comment.find({ post: postId });
+    res.json(comments);
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).json({ error: "Failed to fetch comments" });
+  }
+}
+
+async function addComment(req, res) {
+  try {
+    const { postId, content, username, avtar } = req.body;
+    console.log(username);
+    const newComment = new Comment({
+      author: {
+        username,
+        avtar
+      },
+      content,
+      post: postId,
+    });
+    const savedComment = await newComment.save();
+    res.status(201).json(savedComment);
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    res.status(500).json({ error: "Failed to add comment" });
+  }
+}
+
+async function updateCommentUpvotes(req, res) {
+  try {
+      const { id, userObj } = req.body;
+      const comment = await Comment.findById(id);
+      console.log("comment", comment);
+      if (!comment) {
+          return res.status(404).json({ error: "Comment not found" });
+      }
+      console.log(comment.upvotes)
+      if (comment.upvotes.some((vote) => vote.username == userObj.username)) {
+          return res.status(400).json({ error: "User already upvoted this comment" });
+      }
+
+      if(comment.downvotes.some((vote) => vote.username == userObj.username)){
+        comment.downvotes = comment.downvotes.filter((obj) => obj.username !== userObj.username);
+      }
+      comment.upvotes.push(userObj);
+      await comment.save();
+      res.status(200).json(comment);
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: "Failed to update upvotes for the comment" });
+  }
+}
+
+async function updateCommentDownvotes(req, res) {
+  try {
+      const { id, userObj } = req.body;
+      const comment = await Comment.findById(id);
+      if (!comment) {
+          return res.status(404).json({ error: "Comment not found" });
+      }
+      if (comment.downvotes.some((vote) => vote.username == userObj.username)) {
+          return res.status(400).json({ error: "User already downvoted this comment" });
+      }
+      if(comment.upvotes.some((vote) => vote.username == userObj.username)){
+        comment.upvotes = comment.upvotes.filter((obj) => obj.username !== userObj.username);
+      }
+      comment.downvotes.push(userObj);
+      await comment.save();
+      res.status(200).json(comment);
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: "Failed to update downvotes for the comment" });
+  }
+}
+
+
+module.exports = {
+  getComments,
+  addComment,
+  updateCommentDownvotes,
+  updateCommentUpvotes
+};
